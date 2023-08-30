65fce7248dacb1b6c972399bb54b2374d3d4cf4a
luteroelavoco <luteroelavo90@gmail.com>
2023-08-30 18:44:48 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
+  app.enableCors();
   await app.listen(PORT);
 }
 bootstrap();
